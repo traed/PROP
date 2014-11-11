310267d88ac685e5e0b3c919e47bd269899c05bd
frno0254 <smallgreycreatures@gmail.com>
2014-11-11 09:53:23 +0100

    TextNode.java

---
+++ src/TextNode.java
@@ -0,0 +1,16 @@
+package prop.assignment0;
+
+public class TextNode implements INode{
+
+	@Override
+	public Object evaluate(Object[] args) throws Exception {
+
+		return null;
+	}
+
+	@Override
+	public void buildString(StringBuilder builder, int tabs) {
+		
+		
+	}
+}
