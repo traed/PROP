6f60b61fc807c0e5d862379895fe644f88047274
frno0254 <smallgreycreatures@gmail.com>
2014-11-19 11:05:57 +0100

    Preparations for ass nr 0

---
+++ src/prop/assignment0/Parser.java
@@ -0,0 +1,26 @@
+package prop.assignment0;
+
+import java.io.IOException;
+
+public class Parser implements IParser {
+
+	@Override
+	public void open(String fileName) throws IOException, TokenizerException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public INode parse() throws IOException, TokenizerException,
+			ParserException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
