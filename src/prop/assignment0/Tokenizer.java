0c61bf94bb1af4496661f213915c09df0965d97a
frno0254 <smallgreycreatures@gmail.com>
2014-11-19 14:32:58 +0100

    Tokenizer

---
+++ src/prop/assignment0/Tokenizer.java
@@ -0,0 +1,83 @@
+package prop.assignment0;
+
+import java.io.IOException;
+import java.util.LinkedList;
+
+public class Tokenizer implements ITokenizer {
+	private Scanner scanner;
+	private Lexeme currentLexeme;
+
+	@Override
+	public void open(String fileName) throws IOException, TokenizerException {
+		currentLexeme = null;
+
+		scanner = new Scanner();
+
+		scanner.open(fileName);
+	}
+
+	@Override
+	public Lexeme current() {
+		return currentLexeme;
+	}
+
+	@Override
+	public void moveNext() throws IOException, TokenizerException {
+
+		if(scanner != null) { 
+
+			char currentChar = scanner.current();
+			scanner.moveNext();
+			currentChar = scanner.current(); 
+
+			if(currentChar == scanner.EOF) {
+				currentLexeme = new Lexeme(currentChar+"", Token.EOF);
+
+			} else if(currentChar == '\n') {
+				moveNext();
+
+			} else if(isWhiteSpace(currentChar)) {
+				lookup(currentChar);
+
+			} else {
+				moveNext();	
+			}
+		}
+	}
+
+	@Override
+	public void close() throws IOException {
+		scanner.close();
+	}
+	
+	private void lookup(char currentChar) {
+
+		String matchString = currentChar+"";
+
+		switch(matchString) {
+		case "+": currentLexeme = new Lexeme(matchString, Token.ADD_OP); break;
+		case "=": currentLexeme = new Lexeme(matchString, Token.ASSIGN_OP); break;
+		case "-": currentLexeme = new Lexeme(matchString, Token.SUB_OP); break;
+		case "*": currentLexeme = new Lexeme(matchString, Token.MULT_OP); break;
+		case "/": currentLexeme = new Lexeme(matchString, Token.DIV_OP); break;
+		case "(": currentLexeme = new Lexeme(matchString, Token.LEFT_PAREN); break;
+		case ")": currentLexeme = new Lexeme(matchString, Token.RIGHT_PAREN); break;
+		case ";": currentLexeme = new Lexeme(matchString, Token.SEMICOLON); break;
+		case "{": currentLexeme = new Lexeme(matchString, Token.LEFT_CURLY); break;
+		case "}": currentLexeme = new Lexeme(matchString, Token.RIGHT_CURLY); break;
+
+		default: 
+			if(matchString.matches("[a-z]")) 
+				currentLexeme = new Lexeme(matchString, Token.IDENT);
+
+			else if(matchString.matches("\\d"))
+				currentLexeme = new Lexeme(matchString, Token.INT_LIT);
+
+		}
+	}
+
+	private boolean isWhiteSpace(char currentChar) {
+		return (currentChar != ' ') ? true: false;
+	}
+
+}
