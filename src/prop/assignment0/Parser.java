71d1a22fd3ab3afb860eeef0b4e9ae91a2b1de30
frno0254 <smallgreycreatures@gmail.com>
2014-11-20 12:41:54 +0100

    Parser exception handling...

--- src/prop/assignment0/Parser.java
+++ src/prop/assignment0/Parser.java
@@ -13,50 +13,58 @@ public class Parser implements IParser {
 
 		tokenizer = new Tokenizer();
 		tokenizer.open(fileName);
+		
+		//System.out.println(lexemeList);
 	}
 
 	@Override
 	public INode parse() throws IOException, TokenizerException,
 	ParserException {
-
 		tokenizer.moveNext();
 		INode node = block();
-
+		
 		if(tokenizer.current().token() != Token.EOF)
 			throw new ParserException("Input error. Reached EOF but not EOF token was found.");
 
 		return node;
 	}
 
-	private INode block() throws IOException, TokenizerException {
+	private INode block() throws IOException, TokenizerException, ParserException {
 		//block = '{' + stmts + '}' ;
 	    BlockNode block = new BlockNode();
+	    
 	    if(tokenizer.current().token() == Token.LEFT_CURLY) {
 	    	block.addLexeme(tokenizer.current());
 	    	tokenizer.moveNext();
+	    } else {
+	    	throw new ParserException("ParserException while creating a block node: " + tokenizer.current().token() + " instead of LEFT_CURLY_BRACKETS.");
 	    }
-
 	    block.addChild(stmts());
 
 	    if(tokenizer.current().token() == Token.RIGHT_CURLY) {
 	    	block.addLexeme(tokenizer.current());
 	    	tokenizer.moveNext();
+	    } else {
+	    	throw new ParserException("ParserException while creating a block node: " + tokenizer.current().token() + " instead of RIGHT_CURLY_BRACKETS.");
 	    }
 
 	    return block;
 	}
 
-	private INode stmts() throws IOException, TokenizerException {
+	private INode stmts() throws IOException, TokenizerException, ParserException {
 		//stmts = [ assign, stmts ] ;
 		StatementsNode stmt = new StatementsNode();
+		
 		if(tokenizer.current().token() == Token.IDENT){
 			stmt.addChild(assign());
 			stmt.addChild(stmts());
-		}
+		} /*else {
+			throw new ParserException("ParserException while creating a statements node: " + tokenizer.current().token() + " instead of IDENTIFIER.");
+		}*/
 		return stmt;
 	}
 
-	private INode assign() throws IOException, TokenizerException {
+	private INode assign() throws IOException, TokenizerException, ParserException {
 		//assign = ID, '=', expr, ';' ;
 		AssignmentNode assign = new AssignmentNode();
 		assign.addLexeme(tokenizer.current());
@@ -65,18 +73,23 @@ public class Parser implements IParser {
 		if(tokenizer.current().token() == Token.ASSIGN_OP) {
 			assign.addLexeme(tokenizer.current());
 			tokenizer.moveNext();
+		} else {
+			throw new ParserException("ParserException while creating ass node: " + tokenizer.current().token() + " instead of ASSIGN_OPERATOR.");
 		}
-
+	
 		assign.addChild(expr());
 
 		if(tokenizer.current().token() == Token.SEMICOLON) {
 			assign.addLexeme(tokenizer.current());
 			tokenizer.moveNext();
 		}
+		else {
+			throw new ParserException("ParserException while creating ass node: " + tokenizer.current().token() + " instead of SEMICOLON.");
+		}
 		return assign;
 	}
 
-	private INode expr() throws IOException, TokenizerException {
+	private INode expr() throws IOException, TokenizerException, ParserException {
 		//expr = term, [ ('+' | '-' ) , expr] ;
 		ExpressionNode expr = new ExpressionNode();
 		expr.addChild(term());
@@ -85,35 +98,46 @@ public class Parser implements IParser {
 			tokenizer.moveNext();
 			expr.addChild(expr());
 		}
+		/*else {
+			throw new ParserException("ParserException while creating expression node: " + tokenizer.current().token() + " instead of SUB or ADD op");
+		}*/
 		return expr;
 	}
 
-	private INode term() throws IOException, TokenizerException {
+	private INode term() throws IOException, TokenizerException, ParserException {
 		//term = factor, [ ( '*' | '/' ) , term] ;
 		TermNode term = new TermNode();
 		term.addChild(factor());
+		
 		if(tokenizer.current().token() == Token.MULT_OP || tokenizer.current().token() == Token.DIV_OP) {
 			term.addLexeme(tokenizer.current());
 			tokenizer.moveNext();
 			term.addChild(term());
-		}
+		} 
 		return term;
 	}
 
-	private INode factor() throws IOException, TokenizerException {
+	private INode factor() throws IOException, TokenizerException, ParserException {
 		//factor = INT | ID | '(', expr, ')' ;
 		FactorNode factor = new FactorNode();
 		if(tokenizer.current().token() == Token.INT_LIT || tokenizer.current().token() == Token.IDENT) {
+			System.out.println("hej");
+
 			factor.addLexeme(tokenizer.current());
 			tokenizer.moveNext();
 		} else if(tokenizer.current().token() == Token.LEFT_PAREN) {
+
 			factor.addLexeme(tokenizer.current());
 			tokenizer.moveNext();
 			factor.addChild(expr());
 			if(tokenizer.current().token() == Token.RIGHT_PAREN) {
 				factor.addLexeme(tokenizer.current());
 				tokenizer.moveNext();
+			} else {
+				throw new ParserException("ParserException while creating factor node: " + tokenizer.current().token() + " instead of RIGHT_PARENTHESIS");
 			}
+		} else {
+			throw new ParserException("ParserException while creating factor node: " + tokenizer.current().token() + " instead of LEFT_PARENTHESIS");
 		}
 		return factor;
 	}
@@ -121,5 +145,6 @@ public class Parser implements IParser {
 	@Override
 	public void close() throws IOException {
 	    tokenizer.close();
+	    
 	}
 }
